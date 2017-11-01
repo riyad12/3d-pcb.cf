b39da5135d111b1b493bf79b40b3e35b97833c00
riyad12 <riyadhidayat1993@gmail.com>
2017-11-01 13:17:58 +0700

    Update utama.js

--- utama.js
+++ utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/10/printed-circuit-boards.html"}
 if(x=="2"){xxx="/2017/10/leading-pcb.html"}
 if(x=="3"){xxx="/2017/10/pcb-design.html"}
 if(x=="4"){xxx="/2017/10/pcb-material.html"}
-if(x=="5"){xxx="/2017/10/pcb-manufacturing.html"}a_to_ve[i].href="http://www.3d-pcb.cf"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/pcb-manufacturing.html"}a_to_ve[i].href="http://www.pcb3d.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
