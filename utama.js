b4b5c0bd91eff3a12620e46de9337f7145fdd195
riyad12 <riyadhidayat1993@gmail.com>
2018-09-01 21:53:33 +0700

    Update utama.js

--- utama.js
+++ utama.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/10/printed-circuit-boards.html"}
-if(x=="2"){xxx="/2017/10/leading-pcb.html"}
-if(x=="3"){xxx="/2017/10/pcb-design.html"}
-if(x=="4"){xxx="/2017/10/pcb-material.html"}
-if(x=="5"){xxx="/2017/10/pcb-manufacturing.html"}a_to_ve[i].href="http://www.pcb3d.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/01/five-common-gaps-to-watch-for-in-your.html"}
+if(x=="2"){xxx="/2018/01/tips-to-choose-best-erp-system.html"}
+if(x=="3"){xxx="/2018/01/five-erp-software-trends-to-watch-in.html"}
+if(x=="4"){xxx="/2018/01/the-biggest-changes-to-erp-in-last-5.html"}
+if(x=="5"){xxx="/2018/01/7-ways-your-legacy-erp-system-is.html"}a_to_ve[i].href="http://www.best-erp-software.us"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
